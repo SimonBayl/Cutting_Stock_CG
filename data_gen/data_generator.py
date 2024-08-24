ac9e5caf97fb68eaf821045e9746be3d0a029cb4
SIMON BAYLE <simon.bayle@sia-partners.com>
2024-08-24 23:44:29 +0200

    Major changes in MILP formulation

--- data_gen/data_generator.py
+++ data_gen/data_generator.py
@@ -26,7 +26,32 @@ class data_cs:
         return result
 
 def generate_raw_data(num_pieces):
-    return [(random.uniform(5.0, 75.0), random.randint(20, 50)) for _ in range(num_pieces)]
+    return [(random.uniform(5.0, 40.0), random.randint(20, 35)) for _ in range(num_pieces)]
+
+    # data = [
+    #    (75.0, 38)
+    #     (75.0, 44)
+    #     (75.0, 30)
+    #     (75.0, 41)
+    #     (75.0, 36)
+    #     (53.8, 33)
+    #     (53.0, 36)
+    #     (51.0, 41)
+    #     (50.2, 35)
+    #     (32.2, 37)
+    #     (30.8, 44)
+    #     (29.8, 49)
+    #     (20.1, 37)
+    #     (16.2, 36)
+    #     (14.5, 42)
+    #     (11.0, 33)
+    #     (8.6, 47)
+    #     (8.2, 35)
+    #     (6.6, 49)
+    #     (5.1, 42)
+    # ]
+        
+    # return data
 
 def get_data(num_pieces=20, max_width=100.0):
     raw_data = generate_raw_data(num_pieces)
--- main.py
+++ main.py
@@ -8,17 +8,23 @@ import time
 
 def main():
     """This function runs a benchmark over 2 differents method for a cutting stock problem."""
-    
-    nb_piece, largeur_max = 30, 1000
-    data = get_data(nb_piece,largeur_max)
+
+    nb_rouleaux = 1000
+    nb_piece, largeur_max = 20, 1000
+    data = get_data(nb_piece,largeur_max) # demande_max = 35*nb_pièces*40 <= nb_rouleau*largeur_max
+
+    results_milp = []
+    results_gc = []
 
     t1 = time.time()
-    master_objective, solution_vector = main_gc(data, nb_piece)
+    master_objective, solution_vector = main_gc(data, nb_piece, nb_rouleaux)
     t2 = time.time()
     print(t2-t1)
-    master_objective, solution_vector = classic_MILP(data, nb_piece)
+    print(solution_vector)
+    master_objective, solution_vector = classic_MILP(data, nb_piece, nb_rouleaux)
     t3 = time.time()
     print(t3-t2)
+    # print(solution_vector)
 
 
 if __name__ == "__main__":
